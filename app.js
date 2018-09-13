4cbf2eceeff35c1bdaf07b0bbe26c92d6f5f3a1b
Troi Mendoza <troiraymund@gmail.com>
2018-09-13 15:07:11 +0800

    add db connection

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express  = require("express"),
+      mysql    = require("mysql"),
       response = require('./src/helpers/response'),
       package = require('./package.json');
 
 const app  = express(),
-      port = "3000";
+      port = "3000",
+      db = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'inventory'
+      });
+
+app.listen(port, () => {
+    db.connect(function(err) {
+        if (err) {
+            console.error('error initializing db: ' + err.stack);
+            process.exit();
+        } else {
+            console.log(`Server started: ${package.name}@${package.version}`);
+            console.log(`Listening on port: ${port}`);
+        }
+    });
+});
 
 app.get("/", (req, res, next) => {
     response.success(res, next, {
         service: package.name + '@' + package.version
     }, 200, "running");
-});
-
-app.listen(port, () => {
-    console.log(`Server started: ${package.name}@${package.version}`);
-    console.log(`Listening on port: ${port}`);
 });
 
 process.on('uncaughtException', function (err) {
